621441f35906520917c443269cc4fb12074e1d41
shaniba2021 <saniba1234@gmail.com>
2021-07-13 00:06:12 +0530

    new corrected

--- evaluation_shaniba/models/models.py
+++ evaluation_shaniba/models/models.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from odoo import api, fields, models
+from odoo import fields, models, api
 
 
 class evaluationcustomer(models.Model):
@@ -8,12 +8,42 @@ class evaluationcustomer(models.Model):
      _description = 'evaluation.customer'
 
 
-     name= fields.Char('Customer')
+
+     name = fields.Char('Customer')
      ch_number = fields.Integer('Check Number')
      amount = fields.Integer('Amount')
-     state = fields.Selection([('confirm', 'confirm'), ('cancel', 'cancelled'), ('none', 'none')], default='none')
+     phone = fields.Integer('mobile No')
+     state = fields.Selection([('draft','Draft'),('confirm','Confirmed'),('cancel','Cancelled')],
+                              string='States', default='draft')
+
+     def btn_confirm(self):
+           print("......................................................................................")
+           self.state = 'confirm'
+
+     def btn_cancel(self):
+           print("......................................................................................")
+           self.state = 'cancel'
+
+     def btn_reset(self):
+         print("......................................................................................")
+         self.state = 'draft'
+
+class newcustomer(models.Model):
+     _name = 'new.customer'
+     _description = 'new.customer'
+
+     @api.onchange("cust_name")
+     def onchange_cust_name(self):
+          phone = self.cust_name.phone
+          self.nphone = phone
 
 
-def my_confirm(self):
-     self.state = 'confirm'
+
+     name = fields.Char('customer')
+     nphone = fields.Integer('Mobile No')
+     cust_name = fields.Many2one('evaluation.customer','customer name')
+
+     #def btn_confirm(self):
+         # phone = self.nphone
+
 
